72b28ea07fbe1d5ac933354b94c2cf4024ae86fd
lupin Chen <lupin10502@gmail.com>
2020-01-26 00:23:39 +0800

    add totoal of guess

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,11 @@
 import random
 
-ans = random.randint(1, 100) #設計亂數答案
+ans = random.randint(1, 100) # 設計亂數答案
+count = int(0) #猜謎次數
 
 while True:
 	x = int(input('請輸入數字:'))
+	count += 1
 	if (x - ans) > 0:
 		print('比答案大')
 	elif (x - ans) < 0:
@@ -11,4 +13,5 @@ while True:
 	else:
 		print('終於猜對了!')
 		print('答案是: %d' % ans)
+		print('總共猜了 %d 次' % count)
 		break
